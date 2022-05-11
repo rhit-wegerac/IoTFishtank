a2b8131012dd7b3075235ebea2b2399f8331fd84
rhit-wegerac <wegerac@rose-hulman.edu>
2022-05-10 18:20:51 -0900

    worked on mqtt

--- testmqtt.py
+++ testmqtt.py
@@ -1,1 +1,35 @@
-print("HI")
+#!/usr/bin/python 
+
+import paho.mqtt.client as paho 
+
+  
+
+def on_connect(client, userdata, flags, rc): 
+
+    print("CONNACK received with code %d." % (rc)) 
+
+def on_publish(client, userdata, mid): 
+        pass;
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = paho.Client() 
+
+client.on_publish = on_publish 
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set("fishTank","CptS84fMuQtYBRB") 
+
+client.tls_set(None) 
+
+client.connect("ebcd0562010d4d17b2acce44d85128f1.s1.eu.hivemq.cloud", 8883); 
+
+client.loop_start() 
+
+while True: 
+        
+        (rc, mid) = client.publish("temp/value","Python", qos=1) 
+        
