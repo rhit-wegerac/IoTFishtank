c5055061f31518ac6b345cb52018223f64a47e95
rhit-wegerac <wegerac@rose-hulman.edu>
2022-05-17 05:45:22 -0900

    send photo working

--- testsendimage.py
+++ testsendimage.py
@@ -1,31 +1,46 @@
-
-
-
 import requests
-import json
+from requests.auth import HTTPBasicAuth
+import os
+from time import sleep
 
-url = "https://content.dropboxapi.com/2/files/upload"
+os.system("libcamera-jpeg -o tank.jpg")
+
+url = 'https://smartfish.ddns.net/upload.php'
+files = {'fileimage': open('tank.jpg', 'rb')}
+response=requests.post(url, files=files,auth = HTTPBasicAuth('iot', 'fishtank'))
+print(response)
 
 
-headers = {
-    "Authorization": "Bearer <TOKEN>",
-    "Content-Type": "application/octet-stream",
-    "Dropbox-API-Arg": "{\"path\":\"/image.jpg\",\"mode\":{\".tag\":\"overwrite\"}}"
-}
-data = open("image.jpg","rb").read()
-r = requests.post(url, headers=headers,data=data)
-print(r.content)
 
-url = "https://api.dropboxapi.com/2/files/get_temporary_link"
 
-headers = {
-    "Authorization": "Bearer <TOKEN>",
-    "Content-Type": "application/json"
-}
 
-data = {
-    "path": "/image.jpg"
-}
-
-r = requests.post(url, headers=headers, data=json.dumps(data))
-print(r.content.json())
+##
+##
+#import requests
+#import json
+#
+#url = "https://content.dropboxapi.com/2/files/upload"
+#
+#
+#headers = {
+#    "Authorization": "Bearer <TOKEN>",
+#    "Content-Type": "application/octet-stream",
+#    "Dropbox-API-Arg": "{\"path\":\"/image.jpg\",\"mode\":{\".tag\":\"overwrite\"}}"
+#}
+#data = open("image.jpg","rb").read()
+#r = requests.post(url, headers=headers,data=data)
+#print(r.content)
+#
+#url = "https://api.dropboxapi.com/2/files/get_temporary_link"
+#
+#headers = {
+#    "Authorization": "Bearer <TOKEN>",
+#    "Content-Type": "application/json"
+#}
+#
+#data = {
+#    "path": "/image.jpg"
+#}
+#
+#r = requests.post(url, headers=headers, data=json.dumps(data))
+#print(r.content.json())
