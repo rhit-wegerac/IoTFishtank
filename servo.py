f8659cc4b20672213b5ebdec149db9c4ff992903
rhit-wegerac <34016963+rhit-wegerac@users.noreply.github.com>
2022-05-18 09:33:12 -0400

    Update servo.py

--- servo.py
+++ servo.py
@@ -6,7 +6,8 @@ servo = Servo(17)
 val = -1
 
 x = [0, 20, 40, 80]
-values = [1, 0.56, 0.12, -0.32, -0.76]
+#values = [1, 0.56, 0.12, -0.32, -0.76]
+values = [0.7, 0.26, -0.18, 0.48, -0.78]
 for num in values:
 	servo.value = num
 	print(num)
